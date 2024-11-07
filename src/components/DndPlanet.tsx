08ded8d9c77e8f4e06c16278b4e2470901f6cfd2
gnmjafari <gnm.jafaria@gmail.com>
2024-11-07 22:22:28 +0330

    show planet before ImageLoad

--- src/components/DndPlanet.tsx
+++ src/components/DndPlanet.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { PropsDND } from "../types/types";
+import React, { useState, useEffect } from "react";
 import { DndContext, UniqueIdentifier } from "@dnd-kit/core";
 import { DraggableOverlay, Droppable } from "../../components";
 import { DraggableItem } from "./DraggableItem";
+import { PropsDND } from "../types/types";
 
 const DndPlanet: React.FC<PropsDND> = ({
   collisionDetection,
@@ -11,34 +11,57 @@ const DndPlanet: React.FC<PropsDND> = ({
 }) => {
   const [isDragging, setIsDragging] = useState(false);
   const [parent, setParent] = useState<UniqueIdentifier | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setLoading(false); // تصویر بارگذاری شد
+  };
+
+  useEffect(() => {
+    if (droppableItem?.image) {
+      const img = new Image();
+      img.src = droppableItem.image; // با خیال راحت مقدار را استفاده می‌کنیم
+      img.onload = handleImageLoad; // بعد از بارگذاری تصویر
+    } else {
+      setLoading(false); // اگر تصویر وجود نداشت، بارگذاری را false کنیم
+    }
+  }, [droppableItem?.image]);
 
   return (
-    <DndContext
-      collisionDetection={collisionDetection}
-      modifiers={parent === null ? undefined : modifiers}
-      onDragStart={() => setIsDragging(true)}
-      onDragEnd={({ over }) => {
-        setParent(over ? over.id : null);
-        setIsDragging(false);
-      }}
-      onDragCancel={() => setIsDragging(false)}
-    >
-      {parent === null ? <DraggableItem droppableItem={droppableItem} /> : null}
-
-      <div className={`absolute  ${droppableItem?.position}`}>
-        <Droppable
-          key={droppableItem?.id || "drop_item"}
-          id={droppableItem?.id || "drop_item"}
-          dragging={isDragging}
+    <div>
+      {loading ? (
+        <span className="loading loading-ring loading-lg"></span>
+      ) : (
+        <DndContext
+          collisionDetection={collisionDetection}
+          modifiers={parent === null ? undefined : modifiers}
+          onDragStart={() => setIsDragging(true)}
+          onDragEnd={({ over }) => {
+            setParent(over ? over.id : null);
+            setIsDragging(false);
+          }}
+          onDragCancel={() => setIsDragging(false)}
         >
-          {parent === droppableItem?.id ? (
+          {parent === null ? (
             <DraggableItem droppableItem={droppableItem} />
           ) : null}
-        </Droppable>
-      </div>
 
-      <DraggableOverlay droppableItem={droppableItem} />
-    </DndContext>
+          <div className={`absolute ${droppableItem?.position}`}>
+            <Droppable
+              key={droppableItem?.id || "drop_item"}
+              id={droppableItem?.id || "drop_item"}
+              dragging={isDragging}
+            >
+              {parent === droppableItem?.id ? (
+                <DraggableItem droppableItem={droppableItem} />
+              ) : null}
+            </Droppable>
+          </div>
+
+          <DraggableOverlay droppableItem={droppableItem} />
+        </DndContext>
+      )}
+    </div>
   );
 };
 
