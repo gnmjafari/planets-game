cea3278f578058aea8e00c4e09c99514728e45de
gnmjafari <gnm.jafaria@gmail.com>
2024-11-07 22:15:35 +0330

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import React from "react";
-import DndPlanet from "./components/DndPlanet";
+import React, { lazy, Suspense } from "react";
 import { PLANETS } from "./data/data";
+const DndPlanet = lazy(() => import("./components/DndPlanet"));
 
 const App: React.FC = () => {
   return (
-    <div
-      className="relative bg-sun bg-cover  bg-center w-screen h-svh flex flex-col justify-start items-start"
-      style={{
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
+    <Suspense
+      fallback={
+        <div className="w-screen h-screen flex justify-center items-center">
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      }
     >
-      <div className=" flex-wrap gap-10 bg-center h-svh flex flex-col justify-end items-start p-10">
-        {Object.entries(PLANETS).map(([key, value]) => {
-          return <DndPlanet key={key} droppableItem={value} />;
-        })}
+      <div
+        className="relative bg-sun bg-cover  bg-center w-screen h-svh flex flex-col justify-start items-start"
+        style={{
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className=" flex-wrap gap-5 bg-center h-svh flex flex-col justify-end items-start p-10">
+          {Object.entries(PLANETS).map(([key, value]) => {
+            return <DndPlanet key={key} droppableItem={value} />;
+          })}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
